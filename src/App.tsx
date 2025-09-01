d8c771e688a25d9b7c7a3e9d367a79941a5ad827
linalopes <linalopes@github.com>
2025-09-01 19:44:58 +0200

    Add custom favicon and reorder sections: Workshops now appears before About for better user flow

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
     <div className="min-h-screen bg-white">
       <Header />
       <Hero />
-      <About />
       <Workshops />
+      <About />
       <Agenda />
       <Location />
       <Artist />
